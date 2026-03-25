c8fd6b3627bae4984774e6b802c2ad8f8e4eb7bf
Anastasiia Kurenyova <akurenyova@corelogic.com>
2026-03-25 19:24:01 +0000

    step 4

--- octofit-tracker/backend/octofit_tracker/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/octofit_tracker/settings.py
@@ -27,7 +27,14 @@ SECRET_KEY = 'django-insecure-f32-mpshrp^ua5c0va2%b$0e@#fucty8ycq9*wrd__h)&_*%et
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+
+# Support localhost and Codespace URLs using $CODESPACE_NAME
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}')
 
 
 # Application definition
